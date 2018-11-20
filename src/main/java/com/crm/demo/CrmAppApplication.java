63aa46e0511b82382e7f648d7a2287b5d34bffb6
Jonas Sørensen <jonas7793@gmail.com>
2018-11-20 15:09:35 +0100

    Spring Initializr

---
+++ src/main/java/com/crm/demo/CrmAppApplication.java
@@ -0,0 +1,12 @@
+package com.crm.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrmAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrmAppApplication.class, args);
+    }
+}
